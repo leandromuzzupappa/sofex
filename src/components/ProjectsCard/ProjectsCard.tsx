e3f1628d8dad1e204d979f3088c6304565a6f2b1
CharlesXvr <thestuntman92@gmail.com>
2024-01-12 20:42:48 -0300

    fixed design issues related with padding and box shadow properties

--- src/components/ProjectsCard/ProjectsCard.tsx
+++ src/components/ProjectsCard/ProjectsCard.tsx
@@ -5,44 +5,46 @@ import styles from "./ProjectsCard.module.css";
 const ProjectsCard = () => {
   return (
     <article className={styles.projectsCard}>
-      <div className={styles.infoWrapper}>
-        <Text
-          text={"EcoMarket"}
-          align={"left"}
-          classList={styles.title}
-          color={"black"}
-          weight={"bold"}
-        />
-        <Text
-          text={
-            "Plataforma de comercio electrónico dedicada a la venta de productos ecológicos y sostenibles. Los usuarios pueden explorar y comprar una amplia gama de productos amigables con el medio ambiente."
-          }
-          align={"left"}
-          classList={styles.secondary}
-          color={"black"}
-        />
-        <div className={styles.actionButtons}>
-          <Button
-            text={"Ver el trabajo"}
+      <div className={styles.cardContainer}>
+        <div className={styles.infoWrapper}>
+          <Text
+            text={"EcoMarket"}
+            align={"left"}
+            classList={styles.title}
             color={"black"}
-            icon={IconNames.RIGHTARROW}
-            iconPosition={"right"}
-            variant={"filled"}
-            classList={styles.btnPrimary}
+            weight={"bold"}
           />
-          <Button
-            text={"Mas info"}
+          <Text
+            text={
+              "Plataforma de comercio electrónico dedicada a la venta de productos ecológicos y sostenibles. Los usuarios pueden explorar y comprar una amplia gama de productos amigables con el medio ambiente."
+            }
+            align={"left"}
+            classList={styles.secondary}
             color={"black"}
-            variant={"outlined"}
-            classList={styles.btnSecondary}
+          />
+          <div className={styles.actionButtons}>
+            <Button
+              text={"Ver el trabajo"}
+              color={"black"}
+              icon={IconNames.RIGHTARROW}
+              iconPosition={"right"}
+              variant={"filled"}
+              classList={styles.btnPrimary}
+            />
+            <Button
+              text={"Mas info"}
+              color={"black"}
+              variant={"outlined"}
+              classList={styles.btnSecondary}
+            />
+          </div>
+        </div>
+        <div className={styles.imageWrapper}>
+          <img
+            src="https://cdn.thewirecutter.com/wp-content/media/2022/10/laptopstopicpage-2048px-2102-2x1-1.jpg?auto=webp&quality=75&crop=1.91:1&width=1200"
+            alt=""
           />
         </div>
-      </div>
-      <div className={styles.imageWrapper}>
-        <img
-          src="https://cdn.thewirecutter.com/wp-content/media/2022/10/laptopstopicpage-2048px-2102-2x1-1.jpg?auto=webp&quality=75&crop=1.91:1&width=1200"
-          alt=""
-        />
       </div>
     </article>
   );
