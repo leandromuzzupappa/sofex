dcb0bad2de602607c5537b3893c5b84718947ce3
CharlesXvr <thestuntman92@gmail.com>
2024-01-15 14:58:53 -0300

    added an interface for projects data, and created an array of objects for the data

---
+++ src/data/static/projects.ts
@@ -0,0 +1,36 @@
+interface IProjectsData {
+  projectTitle: string;
+  projectDescription: string;
+  projectUrl: string;
+  projectInfoUrl: string;
+}
+export const projectsData: IProjectsData[] = [
+  {
+    projectTitle: "EcoMarket",
+    projectDescription:
+      "Plataforma de comercio electrónico dedicada a la venta de productos ecológicos y sostenibles. Los usuarios pueden explorar y comprar una amplia gama de productos amigables con el medio ambiente.",
+    projectUrl: "#",
+    projectInfoUrl: "#",
+  },
+  {
+    projectTitle: "TourTracker",
+    projectDescription:
+      "Plataforma interactiva para rastrear y seguir giras de artistas y bandas en todo el mundo. Los usuarios pueden descubrir conciertos, obtener información sobre fechas, lugares y comprar boletos en línea.",
+    projectUrl: "#",
+    projectInfoUrl: "#",
+  },
+  {
+    projectTitle: "FizzFlare",
+    projectDescription:
+      "Esta plataforma ofrece a los amantes de las burbujas una experiencia única, desde la personalización de sabores hasta la participación en eventos exclusivos.",
+    projectUrl: "#",
+    projectInfoUrl: "#",
+  },
+  {
+    projectTitle: "CloudGuard",
+    projectDescription:
+      "Una solución integral diseñada para mantener tus aplicaciones y datos seguros. Desde la detección de amenazas hasta la gestión de accesos, permite aprovechar al máximo los beneficios de la nube sin comprometer la seguridad.",
+    projectUrl: "#",
+    projectInfoUrl: "#",
+  },
+];
