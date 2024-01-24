269138e9c777b119d4c6c7573174b22426268262
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-01-24 01:23:13 -0300

    Add animation to the CardDetails component

--- src/components/CardDetails/CardDetails.tsx
+++ src/components/CardDetails/CardDetails.tsx
@@ -1,6 +1,8 @@
-import { ICardDetails } from "@data/interfaces/Card";
-import styles from "./CardDetails.module.css";
+import { useLayoutEffect, useRef } from "react";
+import gsap from "gsap";
 import { Text } from "@components/Text/Text";
+import styles from "./CardDetails.module.css";
+import { ICardDetails } from "@data/interfaces/Card";
 const CardDetails = ({
   logo,
   company,
@@ -9,17 +11,73 @@ const CardDetails = ({
   name,
   position,
 }: ICardDetails) => {
+  const cardRef = useRef<HTMLDivElement>(null);
+  const imgRef = useRef<HTMLImageElement>(null);
+  const quoteRef = useRef<HTMLQuoteElement>(null);
+  const descriptionRef = useRef<HTMLParagraphElement>(null);
+  const titleRef = useRef<HTMLParagraphElement>(null);
+  const positionRef = useRef<HTMLParagraphElement>(null);
+
+  useLayoutEffect(() => {
+    const tl = gsap.timeline({
+      defaults: { ease: "power3.inOut" },
+      paused: true,
+    });
+
+    tl.fromTo(
+      imgRef.current,
+      { opacity: 0, y: 100 },
+      { opacity: 1, y: 0, duration: 0.5 },
+    );
+    tl.fromTo(
+      [
+        quoteRef.current,
+        descriptionRef.current,
+        titleRef.current,
+        positionRef.current,
+      ],
+      { opacity: 0, y: 20 },
+      { opacity: 1, y: 0, duration: 0.5, stagger: 0.1 },
+      "-=0.3",
+    );
+
+    tl.play();
+
+    return () => {
+      tl.kill();
+    };
+  }, [logo]);
+
   return (
-    <article className={styles.cardDetails}>
-      <header className={styles.imgWrapper}>
-        <img src={logo} alt={company} />
+    <article ref={cardRef} className={styles.cardDetails}>
+      <header
+        className={styles.imgWrapper}
+        style={{
+          backgroundImage: `url(${logo})`,
+          backgroundSize: "2000px",
+        }}
+      >
+        <img ref={imgRef} src={logo} alt={company} />
       </header>
       <main className={styles.content}>
-        <blockquote className={styles.quote}>&quot;{quote}&quot;</blockquote>
-        <Text text={feedback} weight={"regular"} classList={styles.text} />
+        <blockquote ref={quoteRef} className={styles.quote}>
+          &quot;{quote}&quot;
+        </blockquote>
+        <Text
+          selfRef={descriptionRef}
+          text={feedback}
+          weight={"regular"}
+          classList={styles.text}
+        />
         <div className={styles.clientInfo}>
-          <Text text={name} weight={"bold"} classList={styles.title} />
           <Text
+            selfRef={titleRef}
+            text={name}
+            weight={"bold"}
+            classList={styles.title}
+          />
+          <Text
+            selfRef={positionRef}
             text={`${position} at ${company}`}
             classList={styles.secondary}
           />
