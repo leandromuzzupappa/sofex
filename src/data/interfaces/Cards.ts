280d26dba2e711236b3a98edff9719e8115d05a3
Leandro Muzzupappa <leann.muzzu@gmail.com>
2024-01-31 09:13:10 -0300

    Add ICardsSectionProps interface to Cards.ts and update CardsSection component

--- src/data/interfaces/Cards.ts
+++ src/data/interfaces/Cards.ts
@@ -1,4 +1,5 @@
 import { IconNames } from "./Icons";
+import { ITextContentProps } from "./TextContent";
 import { Color } from "@data/types/Colors";
 import { PositionH } from "@data/types/Position";
 
@@ -13,4 +14,14 @@ export interface ICardSmallProps {
   ellipsis?: boolean;
   ellipsisLines?: number;
   ellipsisButtonText?: string;
+}
+
+export interface ICardsSectionProps
+  extends Omit<ITextContentProps, "text">,
+    ICardSmallProps {
+  cards: { [key: string]: string }[];
+  classList?: string;
+  description: string;
+  headline: string;
+  color?: Color;
 }
