fc843ee20a68dd901187be35a50a6db443aebc99
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-02-01 15:40:14 -0300

    created an interface for technologies data and using it in the projects data

--- src/data/static/projects.ts
+++ src/data/static/projects.ts
@@ -4,6 +4,11 @@ interface IProjectsData {
   projectPictureUrl: string;
   projectUrl: string;
   projectInfoUrl: string;
+  projectsTechnologies?: ITechnologiesData[];
+}
+interface ITechnologiesData {
+  techIcon: string;
+  description: string;
 }
 export const projectsData: IProjectsData[] = [
   {
@@ -14,6 +19,31 @@ export const projectsData: IProjectsData[] = [
       "https://images.squarespace-cdn.com/content/v1/606e11b80b94173f33b2dd54/1658325669795-6Z02U7D2MN203TIKG3QQ/james-webb-desktop-background-banner.png",
     projectUrl: "#",
     projectInfoUrl: "#",
+    projectsTechnologies: [
+      {
+        techIcon:
+          "https://imgs.search.brave.com/vW62EhmdMfk7HjQovK_01sgTw5y8V14RQQYtRb1jONQ/rs:fit:860:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy8z/LzMzL0ZpZ21hLWxv/Z28uc3Zn.svg",
+        description:
+          "Figma nos permitió crear prototipos interactivos y diseños visuales colaborativos en tiempo real. Desde la conceptualización de la interfaz hasta la creación de wireframes y prototipos.",
+      },
+      {
+        techIcon: "https://www.svgrepo.com/show/475654/github-color.svg",
+        description:
+          "Utilizamos GitHub como plataforma de control de versiones, lo que facilitó la colaboración entre los miembros del equipo de desarrollo. Uso de función de ramificación y fusión.",
+      },
+      {
+        techIcon:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+        description:
+          "Componentes reutilizables que facilitaron la gestión y actualización de la interfaz. La arquitectura de componentes fue clave para mantener un desarrollo eficiente y escalable.",
+      },
+      {
+        techIcon:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+        description:
+          "Clave para personalizar la apariencia de cada página, garantizando coherencia en la estética del sitio. Desde la selección de colores y fuentes hasta la disposición de los elementos.",
+      },
+    ],
   },
   {
     projectTitle: "TourTracker",
