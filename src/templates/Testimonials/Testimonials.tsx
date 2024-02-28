a26cdddf88894c9bd1836519c087763dae25c525
CarlosOlivera92 <olivera.carlos1999@gmail.com>
2024-02-28 12:38:35 -0300

    mobile version of card details component now fits with figma design

--- src/templates/Testimonials/Testimonials.tsx
+++ src/templates/Testimonials/Testimonials.tsx
@@ -4,6 +4,9 @@ import { ThumbnailCard } from "@components/ThumbnailCard/ThumbnailCard";
 import styles from "./Testimonials.module.css";
 import { IThumbnailData, ICardDetails } from "@data/interfaces/Card";
 import { testimonialsData } from "@data/static/testimonials";
+import { IconNames } from "@data/interfaces/Icons";
+import { Button } from "@components/Button/Button";
+import { useMediaQuery } from "@react-hook/media-query";
 
 export const Testimonials = () => {
   const [selectedItem, setSelectedItem] = useState<IThumbnailData | null>(
@@ -12,6 +15,7 @@ export const Testimonials = () => {
   const [cardDetailsData, setCardDetailsData] = useState<ICardDetails | null>(
     null,
   );
+  const isMobile = useMediaQuery("(max-width: 1024px)");
 
   useEffect(() => {
     if (selectedItem) {
@@ -27,7 +31,7 @@ export const Testimonials = () => {
   const handleCardSelect = (selectedData: IThumbnailData) => {
     setSelectedItem(selectedData);
   };
-
+  console.log(testimonialsData);
   return (
     <section className={styles.testimonialsSection} id="clientes">
       <div className={styles.customersContainer}>
@@ -49,16 +53,53 @@ export const Testimonials = () => {
           )}
         </div>
         <div className={styles.cardDetailsContainer}>
-          {cardDetailsData ? (
-            <CardDetails
-              logo={cardDetailsData.logo}
-              company={cardDetailsData.company}
-              quote={cardDetailsData.quote}
-              feedback={cardDetailsData.feedback}
-              name={cardDetailsData.name}
-              position={cardDetailsData.position}
-            />
+          {testimonialsData && cardDetailsData ? (
+            <>
+              {isMobile ? (
+                testimonialsData.map(
+                  ({ logo, company, quote, feedback, name, position }) => (
+                    <CardDetails
+                      logo={logo}
+                      company={company}
+                      quote={quote}
+                      feedback={feedback}
+                      name={name}
+                      position={position}
+                    />
+                  ),
+                )
+              ) : (
+                <CardDetails
+                  logo={cardDetailsData.logo}
+                  company={cardDetailsData.company}
+                  quote={cardDetailsData.quote}
+                  feedback={cardDetailsData.feedback}
+                  name={cardDetailsData.name}
+                  position={cardDetailsData.position}
+                />
+              )}
+            </>
           ) : null}
+        </div>
+        <div className={styles.controllers}>
+          <Button
+            text={""}
+            theme="primary"
+            variant=""
+            shape="rounded"
+            icon={IconNames.LEFTARROW}
+            iconPosition="left"
+            classList={styles.controller}
+          />
+          <Button
+            text={""}
+            theme="primary"
+            variant=""
+            shape="rounded"
+            icon={IconNames.RIGHTARROW}
+            iconPosition="right"
+            classList={styles.controller}
+          />
         </div>
       </div>
     </section>
